e4c54bfd7bbe6b4b6b61eb36ff09d0af9f6f25c6
luwir100 <luwir100@gmail.com>
2018-02-23 16:07:21 +0100

    first registerUser method ready for testing

--- src/main/java/de/hhu/cs/dbs/internship/project/gui/AuthenticationViewController.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/gui/AuthenticationViewController.java
@@ -29,10 +29,37 @@ public class AuthenticationViewController extends com.alexanderthelen.applicatio
             Project.getInstance().getData().put("email", data.get("email").toString());
         }
         else throw new SQLException(getClass().getName() + "Falsche Eingabe");
+        //  Eigene SQL-Exception werfen, falls Kunde nicht in DB vorhanden? momentan wird SQLException: ResultSet closed geworfen.
     }
 
     @Override
     public void registerUser(Data data) throws SQLException {
-        throw new SQLException(getClass().getName() + ".registerUser(Data) nicht implementiert.");
+        // Schema: Kunde(email,adresseID,vorname,nachname,passwort)
+        String insertKunde = "INSERT INTO Kunde VALUES(?,?,?,?,?)";
+        // Schema: Adresse(ID,plz,ort,straße,hausnummer)
+        String insertAdresse = "INSERT INTO Adresse(plz,ort,straße,hausnummer) VALUES(?,?,?,?)";
+        // TODO: Alle IDs auf AUTOINCREMENT stellen? Die neuste ID wäre dann immer die höchste, solange beide inserts gleichzeitig ausgeführt werden
+
+        PreparedStatement insertKundeStatement = Project.getInstance().getConnection().prepareStatement(insertKunde);
+        PreparedStatement insertAdresseStatement = Project.getInstance().getConnection().prepareStatement(insertAdresse);
+
+        if(!data.get("password1").toString().equals(data.get("password2").toString())){
+            throw new SQLException(getClass().getName() + "Passwörter stimmen nicht überein");
+        }
+
+        insertAdresseStatement.setString(1, data.get("zipCode").toString());
+        insertAdresseStatement.setString(2, data.get("city").toString());
+        insertAdresseStatement.setString(3, data.get("street").toString());
+        insertAdresseStatement.setInt(4, Integer.parseInt(data.get("houseNumber").toString()));
+
+        insertKundeStatement.setString(1, data.get("eMail").toString());
+        insertKundeStatement.setInt(2, insertAdresseStatement.getGeneratedKeys().getInt("ID"));
+        insertKundeStatement.setString(3, data.get("firstName").toString());
+        insertKundeStatement.setString(4, data.get("lastName").toString());
+        insertKundeStatement.setString(5, data.get("password").toString());
+
+        insertKundeStatement.execute();
+        insertAdresseStatement.execute();
+        //throw new SQLException(getClass().getName() + ".registerUser(Data) nicht implementiert.");
     }
 }
